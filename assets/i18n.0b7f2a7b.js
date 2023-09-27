9cb8850207621c360d7d20eb774a25897fc810b5
AVI <andrea.viscardi@gianos.it>
2023-09-27 10:27:48 +0200

    release

--- assets/i18n.0b7f2a7b.js
+++ assets/i18n.0b7f2a7b.js
@@ -1,4 +1,4 @@
-import{d as ct,h as Xt,e as Nn,i as gn,o as Ln,a as pn,r as Ie,c as be,w as Je,F as Ht,g as Kt,b as Tn,f as In,T as bn,j as On}from"./index.3499c90b.js";/*!
+import{d as ct,h as Xt,e as Nn,i as gn,o as Ln,a as pn,r as Ie,c as be,w as Je,F as Ht,g as Kt,b as Tn,f as In,T as bn,j as On}from"./index.b765e8be.js";/*!
   * shared v9.4.0
   * (c) 2023 kazuya kawaguchi
   * Released under the MIT License.
