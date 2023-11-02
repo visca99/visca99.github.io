5226c85db7c6e2c9589aa13a6d4b4dcb9125eaf3
AVI <andrea.viscardi@gianos.it>
2023-11-02 17:06:40 +0100

    release

--- assets/i18n.df549394.js
+++ assets/i18n.df549394.js
@@ -1,4 +1,4 @@
-import{d as ct,h as Xt,e as Nn,i as gn,o as Ln,a as pn,r as Ie,c as be,w as Je,F as Ht,g as Kt,b as Tn,f as In,T as bn,j as On}from"./index.d7a1762a.js";/*!
+import{d as ct,h as Xt,e as Nn,i as gn,o as Ln,a as pn,r as Ie,c as be,w as Je,F as Ht,g as Kt,b as Tn,f as In,T as bn,j as On}from"./index.9b4627ab.js";/*!
   * shared v9.4.0
   * (c) 2023 kazuya kawaguchi
   * Released under the MIT License.
